8f8d345a9c21342704613994d6ca096d53712bd0
ayechanaungdev <ayechanaungdev@gmail.com>
2026-01-25 13:52:36 +0630

    Prepare the screens : 3) Edit the _layout and screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,34 @@
+import { Colors } from "@/constants/theme";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
 
-export default function RootLayout() {
-  return <Tabs />;
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors.primary,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Contacts List",
+          headerTintColor: Colors.primaryDark,
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="person-circle-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="groups"
+        options={{
+          title: "My Groups",
+          headerTintColor: Colors.primaryDark,
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="list" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
---
+++ constants/theme.ts
@@ -0,0 +1,76 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+import { Platform } from "react-native";
+
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
+
+export const Colors = {
+  light: {
+    text: "#11181C",
+    background: "#fff",
+    tint: tintColorLight,
+    icon: "#687076",
+    tabIconDefault: "#687076",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#ECEDEE",
+    background: "#151718",
+    tint: tintColorDark,
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
+    tabIconSelected: tintColorDark,
+  },
+
+  // custom
+  primary: "#6B8E23",
+  primaryDark: "#556B2F",
+  primaryLight: "#9ACD32",
+
+  accent: "#A3B18A",
+  accentLight: "#DAD7CD",
+
+  background: "#F8F9F6",
+  surface: "#FFFFFF",
+  border: "#E5E7EB",
+
+  textPrimary: "#1F2933",
+  textSecondary: "#4B5563",
+  textMuted: "#9CA3AF",
+
+  success: "#6B8E23",
+  danger: "#E11D48",
+  dangerLight: "#FFE4E6",
+
+  shadow: "rgba(0,0,0,0.15)",
+};
+
+export const Fonts = Platform.select({
+  ios: {
+    /** iOS `UIFontDescriptorSystemDesignDefault` */
+    sans: "system-ui",
+    /** iOS `UIFontDescriptorSystemDesignSerif` */
+    serif: "ui-serif",
+    /** iOS `UIFontDescriptorSystemDesignRounded` */
+    rounded: "ui-rounded",
+    /** iOS `UIFontDescriptorSystemDesignMonospaced` */
+    mono: "ui-monospace",
+  },
+  default: {
+    sans: "normal",
+    serif: "serif",
+    rounded: "normal",
+    mono: "monospace",
+  },
+  web: {
+    sans: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif",
+    serif: "Georgia, 'Times New Roman', serif",
+    rounded:
+      "'SF Pro Rounded', 'Hiragino Maru Gothic ProN', Meiryo, 'MS PGothic', sans-serif",
+    mono: "SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+  },
+});

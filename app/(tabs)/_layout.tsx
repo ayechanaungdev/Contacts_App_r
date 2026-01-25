63a7df67cacedb68c29f0ad262c81b9e51b36431
ayechanaungdev <ayechanaungdev@gmail.com>
2026-01-25 14:03:31 +0630

    Develop: Add ‘About’ Screen : 1) add 'i' icon at the header

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Colors } from "@/constants/theme";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { Tabs } from "expo-router";
+import { Link, Tabs } from "expo-router";
+import { Pressable } from "react-native";
 
 export default function TabsLayout() {
   return (
@@ -17,6 +18,20 @@ export default function TabsLayout() {
           tabBarIcon: ({ size, color }) => (
             <Ionicons name="person-circle-outline" size={size} color={color} />
           ),
+          headerRight: () => (
+            <Link href="/about" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <Ionicons
+                    name="information-circle-outline"
+                    size={25}
+                    color={Colors.primaryDark}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
         }}
       />
       <Tabs.Screen
@@ -26,6 +41,20 @@ export default function TabsLayout() {
           headerTintColor: Colors.primaryDark,
           tabBarIcon: ({ size, color }) => (
             <Ionicons name="list" size={size} color={color} />
+          ),
+          headerRight: () => (
+            <Link href="/about" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <Ionicons
+                    name="information-circle-outline"
+                    size={25}
+                    color={Colors.primaryDark}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
           ),
         }}
       />
